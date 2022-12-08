1036f830535fbc7ace30871a985d0dbb1bbb399b
My Name <Me@gmail.com>
2022-12-08 18:40:00 +0400

    task 3 finished

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Задача 1.");
+        int[] myArr = {2, 9, 7, 6, 3, 4, 5, 1};
+        TaskI taskI = new TaskI();
+        taskI.printArrI(myArr);
+        taskI.mergeSortArr(myArr);
+        System.out.println();
+        taskI.printArrII(myArr);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача 2.");
+        TaskII taskII = new TaskII();
+        taskII.delEven();
+
+        System.out.println();
+        System.out.println("Задача 3.");
         TaskIII taskIII = new TaskIII();
         taskIII.MaxMin();
 
-        System.out.println();
-        
-        TaskII taskII = new TaskII();
-        taskII.delEven();
     }
 }
--- TaskI.java
+++ TaskI.java
@@ -1,5 +1,67 @@
-// Реализовать метод сортировки слиянием
+// Реализовать алгоритм сортировки слиянием
 
 public class TaskI {
+    
+    public void mergeSortArr(int[] myArr) {
+        int l = myArr.length;
+        if(l == 1) return;
+        int half = l / 2;
+        int[] left = new int[half];
+        int[] right = new int[l - half];
+
+        for (int i = 0; i < half; i++) {
+            left[i] = myArr[i];
+        }
+        for (int i = 0; i < l - half; i++) {
+            right[i] = myArr[i + half]; 
+        }
+
+        mergeSortArr(left);
+        mergeSortArr(right);
+        merge(myArr, left, right);
+    }
+
+    public void merge(int[] myArr, int[] left, int[] right) {
+        int l = left.length;
+        int r = right.length;
+        int i = 0;
+        int j = 0;
+        int idx = 0;
+
+        while(i < l && j < r) {
+            if(left[i] < right[j]) {
+              myArr[idx] = left[i];
+              i++;
+            } else {
+               myArr[idx] = right[j];
+               j++;
+            }
+            idx++;
+        }
+
+        for (int ll = i; ll < l; ll++ ) {
+            myArr[idx++] = left[ll];
+        }
+
+        for (int rr = j; rr < r; rr++) {
+            myArr[idx++] = right[rr];
+        }
+
+    }
+
+    public void printArrI(int[] myArr) {
+        System.out.print("Исходный массив: ");
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.print(myArr[i] + " ");
+        }
+    }
+
+    public void printArrII(int[] myArr) {
+        System.out.print("Отсортированный массив: ");
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.print(myArr[i] + " ");
+        }
+    }
+
     
 }
