4b94a78850b4db15b7f11e0d440620a033fc6488
My Name <Me@gmail.com>
2022-12-08 16:24:09 +0400

    task 3 completed

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        TaskIII taskIII = new TaskIII();
+        taskIII.MaxMin();
+    }
+}
---
+++ TaskIII.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+/*
+ * Задан целочисленный список ArrayList. 
+ * Найти минимальное, максимальное и среднее из этого списка
+ */
+
+public class TaskIII {
+    ArrayList<Integer> myArray = new ArrayList<>();
+    int max = 0;
+    int min = 0;
+    int count = 0;
+    int result = 0;
+
+    public void MaxMin () {
+        myArray.add(8);
+        myArray.add(23);
+        myArray.add(2);
+        myArray.add(49);
+        myArray.add(7);
+        myArray.add(55);
+
+        for (int i = 0; i < myArray.size(); i++) {
+            if (max < myArray.get(i)) {
+                max = myArray.get(i); 
+            }
+        }
+        min = myArray.get(0);
+        for (int i = 0; i < myArray.size(); i++) {
+            count = count + myArray.get(i);
+            if (myArray.get(i) < min) {
+                min = myArray.get(i);
+            }
+        }
+    
+        System.out.println("Исходная коллекция: " + myArray);
+        System.out.println("Максимальное число: " + max);
+        System.out.println("Минимальное число: " + min);
+        System.out.println("Среднее арифметическое: " + count/myArray.size());
+    }
+    
+    
+
+
+
+
+}
